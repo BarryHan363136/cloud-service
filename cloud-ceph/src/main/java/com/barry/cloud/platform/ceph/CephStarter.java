d9dbfc7215e836c840fac42b8be0ffe9ecf51995
Tongshan Han <Tongshan.Han@partner.bmw.com>
2019-06-11 17:25:53 +0800

    mutil datasource config

---
+++ cloud-ceph/src/main/java/com/barry/cloud/platform/ceph/CephStarter.java
@@ -0,0 +1,24 @@
+package com.barry.cloud.platform.ceph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+/**
+ * TaskApplication [spring boot] 主方法
+ *
+ * 启动方式，右键->run/debug->Spring Boot App
+ *
+ * @author
+ *
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ServletComponentScan
+public class CephStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CephStarter.class, args);
+    }
+
+}
