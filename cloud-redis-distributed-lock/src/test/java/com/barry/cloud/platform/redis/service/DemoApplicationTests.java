036259b5848975184fe688fe7ec01af8e91c1cc8
hts <htbyanghang2@163.com>
2019-06-18 22:51:38 +0800

    spring-security-oauth2 version 2.3.3  -> 2.3.4

--- cloud-redis-distributed-lock/src/test/java/com/barry/cloud/platform/redis/service/DemoApplicationTests.java
+++ cloud-redis-distributed-lock/src/test/java/com/barry/cloud/platform/redis/service/DemoApplicationTests.java
@@ -9,6 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+import java.util.UUID;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
@@ -18,6 +20,7 @@ public class DemoApplicationTests {
 
     private static final String LOCK_NAME = "LOCK_ID";
     private static final String PATH = "TEST_UUID/TEST";
+    private static Integer stockNum = 10;
 
     @Test
     public void testLock01() {
@@ -43,4 +46,29 @@ public class DemoApplicationTests {
             System.out.println("没有拿到锁");
         }
     }
+
+    @Test
+    public void testLock03() {
+        Integer last = 0;
+        Jedis jedis = jedisPool.getResource();
+        for (int i=0;i<100;i++){
+            String uuid = UUID.randomUUID().toString();
+            boolean lockStatus = RedisLockUtils.tryLock(jedis, String.valueOf(i), uuid, 5000);
+            if (!lockStatus) {
+                throw new RuntimeException("第"+i+"个没抢到，换个姿势再试一遍呀");
+            }
+            if (stockNum <= 0) {
+                last = i;
+                System.out.println("========>"+last);
+                //库存不足
+                throw new RuntimeException("活动已经结束,请留意下次活动");
+            } else {
+                stockNum = stockNum - 1;
+            }
+            //解锁
+            RedisLockUtils.release(jedis, String.valueOf(i), uuid);
+        }
+    }
+
+
 }
